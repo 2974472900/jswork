6528fdac6748aac0270a7f85ecdb825c893cb449
2974472900 <2974472900@qq.com>
2019-10-28 11:56:40 +0800

    work5

---
+++ work9/index.js
@@ -0,0 +1,23 @@
+function cal() {
+    let order = document.getElementById('order').value
+    order = parseInt(order) && Number(order)
+    if (!isNaN(order)) {
+        let perbonacci = document.getElementById('Perbonacci')
+        perbonacci.value = recursion(order)
+    } else {
+        alter('请输入数字')
+    }
+ }
+
+ function recursion(n) {
+     if (n < 0) {
+         return '输入数字不能小于0'
+     }else if (n ==0) {
+         return 0
+     } else if (n == 1) {
+         return 1
+     } else if (n >1) {
+         return recursion(n - 1) + recursion(n - 2)
+     }
+}
+ 
