2179d8714d20aaca24fd7270be84e0a1817e02aa
2974472900 <2974472900@qq.com>
2019-11-25 14:37:41 +0800

    work5

--- work12/calendar.js
+++ work12/calendar.js
@@ -3,13 +3,13 @@ function calendar(y) {
     var html = '<div class="box">';
     for (var m = 1; m <= 12; ++m) {
         html += '<table>';
-        html += '<yr class="title"><th colspan="7">' + y +'年' + m + '月</th></tr>';
-        html += '<tr><td>日</td></td>一</td><td>二</td><td>三</td><td>四</td><td>五</td><td>六</td></tr>';
+        html += '<tr class="title"><th colspan="7">' + y +'年' + m + '月</th></tr>';
+        html += '<tr><td>日</td><td>一</td><td>二</td><td>三</td><td>四</td><td>五</td><td>六</td></tr>';
         var max = new Date(y, m, 0).getDate();
         html += '<tr>';
         for (var d = 1; d<= max; ++d){
             if (w && d == 1){
-                html += 'td colspan="' + w + '"></td>';
+                html += '<td colspan="' + w + '"></td>';
             }
             html += '<td>' + d + '</td>';
             if (w == 6 && d != max){
@@ -17,9 +17,11 @@ function calendar(y) {
             }else if (d == max){
                 html += '</tr>';
             }
-            html += '</table>';
+            w = (w + 1 > 6) ? 0 : w + 1; 
         }
-        html += '</div>';
-        return html;
+ 
+        html += '</table>';
     }
+    html += '</div>'
+    return html;
 }
